ff8dc03ca085da9c17d97f31d6470f160b218d14
12ajayvishwa <ajayvish21099@gmail.com>
2023-06-08 19:01:21 +0530

    d

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-//show file list
-const fs = require('fs')
-
+//Crud with file system
+const fs = require('fs');
 const path = require('path');
-const dirPath = path.join(__dirname, 'files');
+const dirPath = path.join(__dirname,'crud');
+const filePath = `${dirPath}/apple.txt`
+
+fs.writeFileSync(filePath,'This is a simple text file');
+fs.readFile(filePath,'utf8',(err,item) => {
+    c
+})
+
+
+
+
+
+//show file list
+// const fs = require('fs')
+
+// const path = require('path');
+// const dirPath = path.join(__dirname, 'files');
 
 // for (i = 0; i < 5; i++) {
 //     fs.writeFileSync(dirPath + "/hello" + i + ".txt", "a simple file");
 // }
 
-fs.readdir(dirPath, (err, files) => {
-    files.forEach((item) => {
-        console.log(item)
-    })
-})
+// fs.readdir(dirPath, (err, files) => {
+//     files.forEach((item) => {
+//         console.log(item)
+//     })
+// })
 
 
 
