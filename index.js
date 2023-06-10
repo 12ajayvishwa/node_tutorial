9f2a627d17387e41cd69d4b9975cc641920e2009
12ajayvishwa <ajayvish21099@gmail.com>
2023-06-10 17:43:18 +0530

    crud append

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 //Crud with file system
 const fs = require('fs');
 const path = require('path');
-const dirPath = path.join(__dirname,'crud');
+const dirPath = path.join(__dirname, 'crud');
 const filePath = `${dirPath}/apple.txt`
 
-fs.writeFileSync(filePath,'This is a simple text file');
-fs.readFile(filePath,'utf8',(err,item) => {
-    c
+//fs.writeFileSync(filePath,'This is a simple text file');
+// fs.readFile(filePath, 'utf8', (err, item) => {
+//     console.log(item);
+// })
+fs.appendFile(filePath, 'and file name is apple.txt', (err) => {
+    if (!err) console.log('file is updated')
 })
 
 
